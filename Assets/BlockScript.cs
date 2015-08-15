e10aa319cb0e736ed645120299dcc2d35b852bc8
Ssteve85uk <stephen.mangold@knights.ucf.edu>
2015-08-15 10:39:18 -0400

    Turret working
    working turret

---
+++ Assets/BlockScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class BlockScript : MonoBehaviour {
+	
+	public int hitsToKill;
+	public int points;
+	private int numberOfHits;
+	
+	// Use this for initialization
+	void Start () {
+		numberOfHits = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision collision){
+		
+		if (collision.gameObject.tag == "Ball"){
+			numberOfHits++;
+			Debug.Log ("hit:"  + numberOfHits);
+			if (numberOfHits == hitsToKill){
+				// destroy the object
+				Destroy(gameObject);
+				Debug.Log ("hit");
+			}
+		}
+	}
+}
